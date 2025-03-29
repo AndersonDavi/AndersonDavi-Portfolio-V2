55e6bf11f659cca7e515bee7b11903691b2ff28c
Anderson Rueda <andesondavid17@gmail.com>
2025-03-29 03:02:22 -0500

    feat: Add draggable functionality to project gallery and enhance project details with new images and links

--- src/app/landing/components/project-gal/project-gal.component.ts
+++ src/app/landing/components/project-gal/project-gal.component.ts
@@ -13,19 +13,79 @@ import { CommonModule } from '@angular/common';
 export class ProjectGalComponent {
   projectsList: ProjectItem[] = [];
   selectedProject: ProjectItem | null = null;
+  private initialX: number = 0;
+  private initialY: number = 0;
+  private currentElement: HTMLElement | null = null;
 
   constructor(private projectService: ProjectsService) {}
 
   ngOnInit(): void {
     this.projectsList = this.projectService.getProjectsList();
     this.selectedProject = this.projectsList[0];
+    this.initDragListeners();
   }
 
   selectProject(project: ProjectItem) {
     this.selectedProject = project;
+    this.resetDraggablePositions();
+  }
+
+  private resetDraggablePositions() {
+    const draggables = document.querySelectorAll('.draggable');
+    draggables.forEach(element => {
+      const el = element as HTMLElement;
+      el.style.position = '';
+      el.style.left = '';
+      el.style.top = '';
+      el.style.transform = '';
+    });
   }
 
   formatTitle(title: string): string {
-    return title.replace(/#(.*?)#/g, '<span class="text-primary-500">$1</span>');
+    return title.replace(
+      /#(.*?)#/g,
+      '<span class="text-primary-500">$1</span>'
+    );
+  }
+
+  private initDragListeners() {
+    document.addEventListener('mousedown', (e) => this.handleDragStart(e));
+    document.addEventListener('mousemove', (e) => this.handleDragMove(e));
+    document.addEventListener('mouseup', () => this.handleDragEnd());
+  }
+
+  private handleDragStart(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains('draggable')) {
+      this.currentElement = target;
+      this.initialX = event.clientX - target.offsetLeft;
+      this.initialY = event.clientY - target.offsetTop;
+      target.style.cursor = 'grabbing';
+    }
+  }
+
+  private handleDragMove(event: MouseEvent) {
+    if (this.currentElement) {
+      event.preventDefault();
+      const x = event.clientX - this.initialX;
+      const y = event.clientY - this.initialY;
+      
+      this.currentElement.style.position = 'absolute';
+      this.currentElement.style.left = `${x}px`;
+      this.currentElement.style.top = `${y}px`;
+    }
+  }
+
+  private handleDragEnd() {
+    if (this.currentElement) {
+      this.currentElement.style.cursor = 'grab';
+      this.currentElement = null;
+    }
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('mousedown', (e) => this.handleDragStart(e));
+    document.removeEventListener('mousemove', (e) => this.handleDragMove(e));
+    document.removeEventListener('mouseup', () => this.handleDragEnd());
   }
 }
--- src/app/landing/services/projects.service.ts
+++ src/app/landing/services/projects.service.ts
@@ -12,7 +12,9 @@ export class ProjectsService {
       description:
         'Colaboración en proyecto ATS para publicación de ofertas de empleo, corrección de estilos y mejoras en diseño',
       demo_url: 'https://www.empleogrupologis.com/',
-      mockup_url: '123',
+      code_url: 'https://github.com/AndersonDavi/GIFS-API',
+      mockup_url:
+        'https://www.figma.com/design/s3O6swXl11T40b6QyVEEIk/ATS?node-id=0-1&t=O7XBCs9gnpOtSysy-1',
       bg_img: 'ATSIMG.webp',
       logo_img: 'ATSLogo.png',
       tecnologies: [
@@ -23,22 +25,46 @@ export class ProjectsService {
         { title: 'TypeScript', img: 'ts.webp' },
       ],
       colors: ['#e45b00', '#ff6e0d', '#f0f0f7'],
-      alt_imgs: ['ATSIMG.webp', 'ATSIMG.webp', 'gifsapp3.webp'],
+      alt_imgs: ['ATS-img-2.png', 'ATS-img-1.png'],
     },
     {
       id: 2,
-      title: 'GIFS#APP2#',
+      title: 'EDICIONES #KAZIYADU#',
       description:
-        'Gestión laboral SAS | Automatizaciòn de procesos con Power Platform, diseño de interfaces, administraciòn de bases de datos SQL SERVER',
-      demo_url: '123',
-      code_url: '123',
-      mockup_url: '',
-      bg_img: 'ATSIMG.webp',
-      logo_img: 'ATSLogo.png',
-
-      tecnologies: [{ title: 'HTML', img: 'html.webp' }],
-      colors: ['#F2F2F2', '#FF5733', '#C70039'],
-      alt_imgs: ['ATSIMG.webp', 'ATSIMG.webp', 'gifsapp3.webp'],
+        'Web para publicar libros gratuitos diseñada para la editorial Ediciones kaziyadu',
+      demo_url: 'https://kaziyadu.vercel.app/',
+      bg_img: 'kaziyaduAppIMG.webp',
+      logo_img: 'kaziyaduAppLogo.png',
+      tecnologies: [
+        { title: 'HTML', img: 'html.webp' },
+        { title: 'CSS', img: 'css.webp' },
+        { title: 'JS', img: 'js.webp' },
+        { title: 'Angular', img: 'angular.webp' },
+        { title: 'TypeScript', img: 'ts.webp' },
+        { title: 'Tailwind', img: 'tailwind.webp' },
+      ],
+      colors: ['#fed801', '#eb546c', '#84bae7'],
+      alt_imgs: ['kaziyadu-img-1.png', 'kaziyadu-img-2.png'],
+    },
+    {
+      id: 3,
+      title: 'GIFS#APP#',
+      description:
+        'Proyecto personal como práctica de Angular, buscador de Gifs con historial y conexión a la API de Giphy',
+      demo_url: 'https://andersondavi.github.io/GIFS-API/',
+      code_url: 'https://github.com/AndersonDavi/GIFS-API',
+      bg_img: 'gifsAppIMG.webp',
+      logo_img: 'gifsAppLogo.png',
+      tecnologies: [
+        { title: 'HTML', img: 'html.webp' },
+        { title: 'CSS', img: 'css.webp' },
+        { title: 'JS', img: 'js.webp' },
+        { title: 'Angular', img: 'angular.webp' },
+        { title: 'TypeScript', img: 'ts.webp' },
+        { title: 'Bootstrap', img: 'bootstrap.webp' },
+      ],
+      colors: ['#212529', '#1f2022', '#ffffff'],
+      alt_imgs: ['GIFS-img-1.png', 'GIFS-img-2.png'],
     },
   ];
 
